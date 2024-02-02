eea3f8cb02e3bb69855669c6153fbdcf0cea3501
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-02-02 15:51:17 +0100

    Partial write to hdf5 file

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -3,8 +3,12 @@
 #include <H5Cpp.h>
 #include <queue>
 #include <cassert>
+#include <filesystem>
+#include <string>
+#include <vector>
 
 using namespace std;
+using namespace std::filesystem;
 
 typedef pair<int, int> result_t;
 
@@ -13,6 +17,8 @@ const uint32_t m2  = 0x33333333; //binary: 00110011..
 const uint32_t m4  = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
 const uint32_t m8  = 0x00ff00ff; //binary:  8 zeros,  8 ones ...
 const uint32_t m16 = 0x0000ffff; //binary: 16 zeros, 16 ones ...
+								 //
+const vector<string> sizes{"10K", "100K", "1M", "30M", "10M", "100M"};
 
 /* 
  * See wiki page for Hamming weight
@@ -68,6 +74,9 @@ int main(int argc, char *argv[]) {
 		.default_value(30)
 		.scan<'i', int>()
 		.help("Number of nearest neighbors to be found");
+	program.add_argument("--size", "-s")
+		.required()
+		.help("Size of the dataset");
 
 	try {
 		program.parse_args(argc, argv);
@@ -81,10 +90,17 @@ int main(int argc, char *argv[]) {
 	auto datafile = program.get<string>("--datafile");
 	auto queryfile = program.get<string>("--queryfile");
 	auto k = program.get<int>("-k");
+	auto size = program.get<string>("--size");
+
+	assert(filesystem::exists(datafile));
+	assert(filesystem::exists(queryfile));
+	assert(k > 0);
+	assert(find(sizes.begin(), sizes.end(), size) != sizes.end());
 
 	cout << "Reading data from file: " << datafile << endl;
 	cout << "Reading queries from file: " << queryfile << endl;
 	cout << "Finding " << k << " nearest neighbors" << endl;
+	cout << "Size of the dataset: " << size << endl;
 
 
 	H5::H5File h5datafile(datafile, H5F_ACC_RDONLY);
@@ -118,10 +134,45 @@ int main(int argc, char *argv[]) {
 
 	cout << "Files read successfully" << endl;
 
-	result_t *result = bruteforce(data, rows, cols, k, queries);
+	int knns[query_rows];
+	int dists[query_rows];
 
-	cout << "Nearest neighbors found:" << endl;
-	for (int i = 0; i < k; i++) {
-		cout << result[i].second << " with distance " << result[i].first << endl;
+	for (int i = 0; i < query_rows; i++) {
+		if (i % 100 == 0) {
+			cout << "Processed " << i << " queries" << endl;
+		}
+		result_t *result = bruteforce(data, rows, cols, k, &queries[i * query_cols]);
+		for (int j = 0; j < k; j++) {
+			dists[i * query_cols + j] = result[j].first;
+			knns[i * query_cols + j] = result[j].second + 1;
+		}
 	}
+
+	if (!filesystem::exists("results/"))
+		filesystem::create_directory("results/");
+
+	H5::H5File result_file("results/result.h5", H5F_ACC_TRUNC);
+	
+	H5::Attribute data_attr = result_file.createAttribute("data", H5::StrType(H5::PredType::C_S1, 256), H5::DataSpace(H5S_SCALAR));
+	data_attr.write(H5::StrType(H5::PredType::C_S1, 256), "hamming");
+/*
+	H5::Attribute size_attr = result_file.createAttribute("size", H5::PredType::NATIVE_INT, H5::DataSpace(H5S_SCALAR));
+	size_attr.write(H5::StrType(H5::PredType::C_S1), string{size}.c_str());
+*/
+	H5::Attribute algo_attr = result_file.createAttribute("algo", H5::StrType(H5::PredType::C_S1, 256), H5::DataSpace(H5S_SCALAR));
+	algo_attr.write(H5::StrType(H5::PredType::C_S1, 256), "bruteforce");
+
+	H5::Attribute buildtime_attr = result_file.createAttribute("buildtime", H5::PredType::NATIVE_FLOAT, H5::DataSpace(H5S_SCALAR));
+	buildtime_attr.write(H5::PredType::NATIVE_FLOAT, "0.0");
+
+	H5::Attribute querytime_attr = result_file.createAttribute("querytime", H5::PredType::NATIVE_FLOAT, H5::DataSpace(H5S_SCALAR));
+	querytime_attr.write(H5::PredType::NATIVE_FLOAT, "0.0");
+
+	H5::Attribute params_attr = result_file.createAttribute("params", H5::StrType(H5::PredType::C_S1, 256), H5::DataSpace(H5S_SCALAR));
+	params_attr.write(H5::StrType(H5::PredType::C_S1, 256), to_string(k).c_str());
+
+	hsize_t knn_dims[2] = {query_rows, k};
+	H5::DataSet knn_set = result_file.createDataSet("knn", H5::PredType::NATIVE_INT, H5::DataSpace(2, knn_dims));
+	
+	knn_set.write(knns, H5::PredType::NATIVE_INT);
 }
